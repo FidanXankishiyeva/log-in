690885dbd06c727c05aae7f0d3a6293a92ec7304
FidanXankishiyeva <fidan.xankisiyev06@gmail.com>
2024-03-10 14:14:11 +0400

    add success page

--- page/loginPage/index.js
+++ page/loginPage/index.js
@@ -1,41 +1,62 @@
-let hideIcon=document.querySelector('.bi-eye-slash')
-let showIcon=document.querySelector('.bi-eye')
-let passwordInput=document.querySelector('.password-input')
+let hideIcon = document.querySelector(".bi-eye-slash");
+let showIcon = document.querySelector(".bi-eye");
+let passwordInput = document.querySelector(".password-input");
+let form = document.querySelector("form");
+let emailInput = document.querySelector(".email-input");
+let errorMsj = document.querySelector(".error-msj");
+let notFoundError = document.querySelector(".not-found-error");
+let userExist = false;
 
-// show password action 
-hideIcon.addEventListener('click', showPass)
-function showPass(){
-    passwordInput.type= 'text'
-    hideIcon.style.display= 'none'
-    showIcon.style.display= 'block'
+// show password action
+hideIcon.addEventListener("click", showPass);
+function showPass() {
+  passwordInput.type = "text";
+  hideIcon.style.display = "none";
+  showIcon.style.display = "block";
 }
 
 // hide password action
 
-showIcon.addEventListener('click', hidePass)
-function hidePass(){
-    passwordInput.type= 'password'
-    hideIcon.style.display= 'block'
-    showIcon.style.display= 'none'
+showIcon.addEventListener("click", hidePass);
+function hidePass() {
+  passwordInput.type = "password";
+  hideIcon.style.display = "block";
+  showIcon.style.display = "none";
 }
 
+// handle login
+form.addEventListener("submit", handleLogin);
+function handleLogin(e) {
+  e.preventDefault();
+  // console.log("lkjh");
+  if (emailInput.value && passwordInput.value) {
+    errorMsj.style.display = "none";
+    findUser();
+  } else {
+    errorMsj.style.display = "block";
+  }
+}
 
-
-// userin elave olunmasi 
-let form= document.querySelector('form')
-let emailInput= document.querySelector('.email-input')
-// let users= []
-// localStorage.setItem('myusers', JSON.stringify(users))
-form.addEventListener('submit',addUser)
-function addUser(e){
-    e.preventDefault()
-    let user= {
-        email: emailInput.value,
-        password:passwordInput.value
+// find user
+function findUser() {
+  let users = localStorage.getItem("users");
+  let usersData = JSON.parse(users);
+  for (let index = 0; index < usersData.length; index++) {
+    if (
+      usersData[index].email == emailInput.value &&
+      usersData[index].password == passwordInput.value
+    ) {
+      userExist = true;
+      break;
+    } else {
+      userExist = false;
     }
-    let userArray= localStorage.getItem('myusers')
-    let userArray2= JSON.parse(userArray)
-    userArray2.push(user)
-    localStorage.setItem('myusers', JSON.stringify(userArray2))
-    form.reset()
+  }
+
+  if (userExist) {
+    notFoundError.style.display = "none";
+    window.location.href = "../homePage/index.html";
+  } else {
+    notFoundError.style.display = "block";
+  }
 }
--- page/signupPage/index.js
+++ page/signupPage/index.js
@@ -1,37 +1,82 @@
-let hideIcon=document.querySelector('.bi-eye-slash')
-let showIcon=document.querySelector('.bi-eye')
-let passwordInput=document.querySelector('.password-input')
-let btn=document.querySelector('.create-btn')
-let emailInput =document.querySelector('.email-input')
-let form =document.querySelector('form')
+let hideIcon = document.querySelector(".bi-eye-slash");
+let showIcon = document.querySelector(".bi-eye");
+let passwordInput = document.querySelector(".password-input");
+let btn = document.querySelector(".create-btn");
+let emailInput = document.querySelector(".email-input");
+let form = document.querySelector("form");
+let errorMsj = document.querySelector(".error-msj-1");
+let userExist = false;
+let existMsj = document.querySelector(".error-msj-2");
 
-
-
-
-// show password action 
-hideIcon.addEventListener('click', showPass)
-function showPass(){
-    passwordInput.type= 'text'
-    hideIcon.style.display= 'none'
-    showIcon.style.display= 'block'
+// show password action
+hideIcon.addEventListener("click", showPass);
+function showPass() {
+  passwordInput.type = "text";
+  hideIcon.style.display = "none";
+  showIcon.style.display = "block";
 }
 
 // hide password action
 
-showIcon.addEventListener('click', hidePass)
-function hidePass(){
-    passwordInput.type= 'password'
-    hideIcon.style.display= 'block'
-    showIcon.style.display= 'none'
+showIcon.addEventListener("click", hidePass);
+function hidePass() {
+  passwordInput.type = "password";
+  hideIcon.style.display = "block";
+  showIcon.style.display = "none";
 }
 
+// database yaratmaq
 
-// input value action 
-form.addEventListener('submit', createBtn)
-function createBtn(e){
-    e.preventDefault()
-console.log(emailInput.value);
-console.log(passwordInput.value);
+function createDatabase() {
+  let user = [];
+  localStorage.setItem("users", JSON.stringify(user));
+}
+// createDatabase()
 
+// handle register
+form.addEventListener("submit", handleRegister);
+function handleRegister(e) {
+  e.preventDefault();
+  if (emailInput.value && passwordInput.value) {
+    checkUser();
+    errorMsj.style.display = "none";
+  } else {(errorMsj.style.display = "block")};
 }
 
+// istifadecinin yoxlanilmasi
+
+function checkUser() {
+  let users = localStorage.getItem("users");
+  let userData = JSON.parse(users);
+  for (let index = 0; index < userData.length; index++) {
+    if (userData[index].email == emailInput.value) {
+      userExist = true;
+      break;
+    } else {
+      userExist = false;
+    }
+  }
+
+  if (userExist) {
+    existMsj.style.display = "block";
+  } else {
+    existMsj.style.display = "none";
+    createUser();
+    window.location.href = "../loginPage/index.html";
+  }
+}
+
+// istifadecinin yaradilmasi
+function createUser() {
+  let users = localStorage.getItem("users");
+  let usersData = JSON.parse(users);
+  let user = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  usersData.push(user);
+
+  localStorage.setItem("users", JSON.stringify(usersData));
+  form.reset();
+}
